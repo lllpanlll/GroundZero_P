c0577d610a150360ebf35dae4b7fc845688c9584
llldalll <ta98321@naver.com>
2016-04-06 00:37:19 +0900

    미니맵v2를 붙여보았다

---
+++ GroundZero_P/Assets/02.Scripts/UI/UI_IconObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_IconObject : MonoBehaviour {
+
+    public GameObject oIconObjPref;
+    GameObject oUIMap;
+    GameObject oPlayer;
+
+    float oX, oZ, objX, objZ;
+
+    void Awake()
+    {
+        oPlayer = GameObject.FindGameObjectWithTag(Tags.Player);
+        oUIMap = GameObject.Find("Center_UI"); // warning
+        oIconObjPref = Instantiate(oIconObjPref) as GameObject;
+    }
+
+    void Start ()
+    {
+        oIconObjPref.transform.parent = oUIMap.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // obj
+        oX = transform.position.x - oPlayer.transform.position.x;
+        oZ = transform.position.z - oPlayer.transform.position.z;
+        objX = (oX * 5f);
+        objZ = (oZ * 5f);
+        oIconObjPref.transform.localPosition = new Vector3(objX, objZ);
+        oIconObjPref.transform.rotation = Quaternion.Euler(0, 0, -oUIMap.transform.rotation.z);
+    }
+}
